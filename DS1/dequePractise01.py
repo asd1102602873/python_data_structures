88c276b9b3a5828c03943cf95ae5c0657a716fb4
lv <1102602873@email.com>
2020-03-06 19:57:56 +0800

    递归

--- DS1/dequePractise01.py
+++ DS1/dequePractise01.py
@@ -1,6 +1,27 @@
+# class Deque:
+#     def __init__(self):
+#         self.items = []
+
+#     def isEmpty(self):
+#         return self.items == []
+
+#     def addFront(self, item):
+#         self.items.append(item)
+
+#     def addRear(self, item):
+#         self.items.insert(0,item)
+
+#     def removeFront(self):
+#         return self.items.pop()
+
+#     def removeRear(self):
+#         return self.items.pop(0)
+
+#     def size(self):
+#         return len(self.items)
 
 
-#i
+# from collections import deque
 from pythonds.basic.deque import Deque
 
 def palChecker(aString):
